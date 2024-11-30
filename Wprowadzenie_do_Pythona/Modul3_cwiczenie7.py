23094f25eb91ed0ffdef3e2d1de615c083c67def
MSz-HA <milosz.szewczyk@gmail.com>
2024-11-30 16:38:18 +0100

    Ćwiczenia z modułu 3

--- Wprowadzenie_do_Pythona/Modul3_cwiczenie7.py
+++ Wprowadzenie_do_Pythona/Modul3_cwiczenie7.py
@@ -3,8 +3,7 @@ import string
 
 
 text = input('Podaj dowolny łańcuch znaków:\n')
-text_set = set(text)
-text_letters = text_set & set(string.ascii_lowercase)
-text_digits = text_set & set(string.digits)
-print(f"W zdaniu '{text}' występuje {len(text_letters)} znaków wspólnych ze zbiorem string.ascii_lowercase, co stanowi {len(text_letters)/len(string.ascii_lowercase):.2%} % tego zbioru.")
-print(f"W zdaniu '{text}' występuje {len(text_digits)} znaków wspólnych ze zbiorem string.ascii_lowercase, co stanowi {len(text_digits)/len(string.digits):.2%} % tego zbioru.")
+text_letters = set(text.lower()) & set(string.ascii_lowercase)
+text_digits = set(text.lower()) & set(string.digits)
+print(f"W zdaniu '{text}' występuje {len(text_letters)} znaków wspólnych ze zbiorem string.ascii_lowercase, co stanowi {len(text_letters)/len(string.ascii_lowercase):.2%} tego zbioru.")
+print(f"W zdaniu '{text}' występuje {len(text_digits)} znaków wspólnych ze zbiorem string.ascii_lowercase, co stanowi {len(text_digits)/len(string.digits):.2%} tego zbioru.")
